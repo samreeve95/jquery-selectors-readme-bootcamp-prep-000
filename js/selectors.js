7ca6c78e2ea628d53240544dc0a20ff630abbfa8
Sam Reeve <sam.reeve95@gmail.com>
2019-04-28 04:23:11 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+
+function paragraphSelector()
+{
+  return $('p');
+}
+
+function lastImageSelector()
+{
+  return $('img:last-child');
+}
+
+function ninjaBabySelector()
+{
+  
+}
